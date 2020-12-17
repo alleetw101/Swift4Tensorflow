dee9a0c339975293227b239d7fbb3dee6396199e
Alan Lee <alleetw101@gmail.com>
2020-12-17 10:09:24 -0500

    import data for modelTrainingWalkthrough

--- Practice/ModelTrainingWalkthrough.swift
+++ Practice/ModelTrainingWalkthrough.swift
@@ -7,7 +7,29 @@
 
 import TensorFlow
 import PythonKit
+import Foundation
 
+/// Model example from Tensorflow site
 func modelTrainingWalkthrough() {
+    let plt = Python.import("matplotlib.pyplot")
+    
+    // Function to download file (Doesn't work with provided csv link)
+    func download(from sourceString: String, to destinationString: String) {
+        let source = URL(string: sourceString)!
+        let destination = URL(fileURLWithPath: destinationString)
+        print(source)
+        let data = try! Data.init(contentsOf: source)
+        try! data.write(to: destination)
+    }
+    
+    let trainDataFilename = "/Users/alan/Documents/Programming/Swift/Swift4Tensorflow/Practice/iris_training.csv"
+//    download(from: "http://download.tensorflow.org/data/iris_training.csv", to: trainDataFilename)
+    
+    let f = Python.open(trainDataFilename)
+    for _ in 0..<5 {
+        print(Python.next(f).strip())
+    }
+    f.close()
+    
     
 }
