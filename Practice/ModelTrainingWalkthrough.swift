45ba49c73babf8402e4474ef423241b932cf2106
Alan Lee <alleetw101@gmail.com>
2020-12-19 08:28:53 -0500

    Reorganization of stored properties and addition of ModelTrainingWalkthrough.visualizeProcesses

--- Practice/ModelTrainingWalkthrough.swift
+++ Practice/ModelTrainingWalkthrough.swift
@@ -10,15 +10,30 @@ import PythonKit
 import Foundation
 
 /// Model example from Tensorflow site
+///
+/// Training dataset from http://download.tensorflow.org/data/iris_training.csv
 class ModelTrainingWalkthrough {
+    // Data file path
     let trainDataFilename = "/Users/alan/Documents/Programming/Swift/Swift4Tensorflow/Practice/iris_training.csv"
+    
+    // CSV column and label information
+    let featureNames = ["sepal_length", "sepal_width", "petal_length", "petal_width"]
+    let labelName = "species"
+    let classNames = ["Iris setosa", "Iris versicolor", "Iris virginica"]
+    
+    // Training dataset list from preProcessData() from csv
     var trainingDataset: [IrisBatch] {get {preProcessData()} set {}}
     
-    init() {
-    }
+    // Model
+    var model = IrisModel()
     
+    // Training stats
+    var trainAccuracyResults: [Float] = []
+    var trainLossResults: [Float] = []
+    
+    
+    // Function to download file (Doesn't work with provided csv link)
     func downloadData() {
-        // Function to download file (Doesn't work with provided csv link)
         func download(from sourceString: String, to destinationString: String) {
             let source = URL(string: sourceString)!
             let destination = URL(fileURLWithPath: destinationString)
@@ -30,24 +45,9 @@ class ModelTrainingWalkthrough {
     //    download(from: "http://download.tensorflow.org/data/iris_training.csv", to: trainDataFilename)
     }
     
+    // Creating dataset using Epochs API.
     func preProcessData() -> [IrisBatch] {
-        // Examine csv
-        let f = Python.open(trainDataFilename)
-        for _ in 0..<5 {
-            print(Python.next(f).strip())
-        }
-        f.close()
-        
-        // CSV column and label information
-        let featureNames = ["sepal_length", "sepal_width", "petal_length", "petal_width"]
-        let labelName = "species"
-        let columnNames = featureNames + [labelName]
-        let classNames = ["Iris setosa", "Iris versicolor", "Iris virginica"]
-        
-        // Creating dataset using Epochs API.
-        let batchsize = 32
-        
-        // Initialize an IrisBatch dataset from a csv file
+        // Initialize an IrisBatch dataset from a csv file.
         func loadIrisDatasetFromCSV(contentsOf: String, hasHeader: Bool, featureColumns: [Int], labelColumns: [Int]) -> [IrisBatch] {
             let np = Python.import("numpy")
             
@@ -66,10 +66,47 @@ class ModelTrainingWalkthrough {
         return loadIrisDatasetFromCSV(contentsOf: trainDataFilename, hasHeader: true, featureColumns: [0, 1, 2, 3], labelColumns: [4])
     }
     
-    func trainModel(batchsize: Int = 32) {
-        let plt = Python.import("matplotlib.pyplot")
+    func trainModel(batchsize: Int = 32, epochCount: Int = 500) {
         let trainingEpochs = TrainingEpochs(samples: trainingDataset, batchSize: batchsize)
+        let columnNames = featureNames + [labelName]
+        
+        trainAccuracyResults = []
+        trainLossResults = []
+        
+        let optimizer = SGD(for: model, learningRate: 0.01)
+        
+        func accuracy(predictions: Tensor<Int32>, truths: Tensor<Int32>) -> Float {
+            return Tensor<Float>(predictions .== truths).mean().scalarized()
+        }
+        
+        
+        
+    }
+        
+    func visualizeProcesses() {
+        // Examine csv
+        let f = Python.open(trainDataFilename)
+        for _ in 0..<5 {
+            print(Python.next(f).strip())
+        }
+        f.close()
+        
+        let trainingEpochs = TrainingEpochs(samples: trainingDataset, batchSize: 32)
+        
+        let firstTrainEpoch = trainingEpochs.next()!
+        let firstTrainBatch = firstTrainEpoch.first!.collated
+        let firstTrainFeatures = firstTrainBatch.features
+        let firstTrainLabels = firstTrainBatch.labels
+        
+        let plt = Python.import("matplotlib.pyplot")
+        let firstTrainFeaturesTransposed = firstTrainFeatures.transposed()
+        let petalLengths = firstTrainFeaturesTransposed[2].scalars
+        let sepalLengths = firstTrainFeaturesTransposed[0].scalars
 
+        plt.scatter(petalLengths, sepalLengths, c: firstTrainLabels.array.scalars)
+        plt.xlabel("Petal length")
+        plt.ylabel("Sepal length")
+        plt.show()
     }
 }
 
@@ -88,5 +125,16 @@ extension IrisBatch: Collatable {
         // IrisBatch'es are collated by stacking their feature and label tensors along the batch axis to produce a single feature and label tensor
         features = Tensor<Float>(stacking: samples.map{$0.features})
         labels = Tensor<Int32>(stacking: samples.map{$0.labels})
+    }
+}
+
+let hiddensize: Int = 10
+struct IrisModel: Layer {
+    var layer1 = Dense<Float>(inputSize: 4, outputSize: hiddensize, activation: relu)
+    var layer2 = Dense<Float>(inputSize: hiddensize, outputSize: hiddensize, activation: relu)
+    var layer3 = Dense<Float>(inputSize: hiddensize, outputSize: 3, activation: relu)
+    
+    func callAsFunction(_ input: Tensor<Float>) -> Tensor<Float> {
+        return input.sequenced(through: layer1, layer2, layer3)
     }
 }
--- Swift4Tensorflow/main.swift
+++ Swift4Tensorflow/main.swift
@@ -10,6 +10,10 @@ import Foundation
 print("Hello, World!")
 // generalTest()
 
+//var PracticeScratch = ExampleClass()
+//print(PracticeScratch.exampleVariable)
+//PracticeScratch.exampleVariable = "String"
+//print(PracticeScratch.exampleVariable)
+
 var sample_model = ModelTrainingWalkthrough()
 sample_model.trainModel()
-print(sample_model.trainingDataset)
